1c54c431b0b43cd830fc8e3fe42cbd9e659eba50
浅梦凉城 <m17762618644@164.com>
2021-07-14 22:19:59 +0800

    初始化

---
+++ java/src/main/java/xyz/esion/manage/ManageApplication.java
@@ -0,0 +1,19 @@
+package xyz.esion.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+
+/**
+ * @author Esion
+ * @since 2021/6/20
+ */
+@SpringBootApplication
+public class ManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageApplication.class, args);
+    }
+
+}
